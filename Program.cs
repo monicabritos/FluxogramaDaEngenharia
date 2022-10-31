bf89e1270074dcf249d96c9b7404ed1deb71de41
Mônica <monicambs@1@gmail.com>
2022-10-31 15:04:03 -0300

    ssdd

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---\n");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "Bom!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Bom!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
